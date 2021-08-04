eb1eda35d22320c7fe94d1f400f6342bb7074282
stanislav.bigunenko <stas.bigunenko@gmail.com>
2021-08-04 15:20:25 +0300

    changed Update function

--- storage/inMemory/inMemory.go
+++ storage/inMemory/inMemory.go
@@ -61,8 +61,7 @@ func (s *Storage) Update(id int, p model.Post) (model.Post, error) {
 	if !ok {
 		return model.Post{}, errors.New("Post cann't be updated - Id not found")
 	}
-	p.Id = id
-	s.Storage[p.Id] = p
+	s.Storage[id] = p
 	return p, nil
 }
 
