5b6f94ccb9da668aaafe75573dc37b64000049df
stanislav.bigunenko <stas.bigunenko@gmail.com>
2021-08-13 18:08:06 +0300

    Trying with tests... doesn't work

--- pkg/handlers/handlers_test.go
+++ pkg/handlers/handlers_test.go
@@ -2,13 +2,14 @@ package handlers
 
 import (
 	"bytes"
+	"github.com/gorilla/mux"
 	"net/http"
 	"net/http/httptest"
+	"src/http/cmd"
 	"src/http/pkg/model"
-	"testing"
-
 	"src/http/pkg/services"
 	"src/http/storage"
+	"testing"
 )
 
 var p = model.Post{}
@@ -16,6 +17,7 @@ var p = model.Post{}
 func TestPostHandler(t *testing.T) {
 	type fields struct {
 		s storage.Storage
+		r mux.Router
 	}
 
 	type args struct {
@@ -65,9 +67,12 @@ func TestPostHandler(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			srv := services.NewStore(tt.fields.s)
+			server := cmd.New()
+			rrv := server.GetRouter()
 
 			var s = &PostHandler{
 				services: srv,
+				router:   &rrv,
 			}
 
 			hs := httptest.NewServer(s.Routes())
